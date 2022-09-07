18c1fdd44b2a19698e3996e07eb8ae07ce0ec542
Peter Salvi <vukung@yahoo.com>
2022-09-07 14:12:06 +0200

    BSpline curve fitter (not tested!)

--- bspline-fit.cc
+++ bspline-fit.cc
@@ -9,6 +9,66 @@ using namespace Geometry;
 using IndexPair = std::pair<size_t, size_t>;
 using VecMap = Eigen::Map<const Eigen::Vector3d>;
 
+void bsplineFit(BSCurve &curve, size_t resolution, const PointVector &samples,
+                const std::function<bool(size_t)> &isFixed, double smoothness) {
+  const auto &basis = curve.basis();
+  size_t p = basis.degree();
+  auto &cpts = curve.controlPoints();
+  auto n = cpts.size();
+
+  std::map<size_t, size_t> index_map;
+  size_t index = 0;
+  for (size_t i = 0; i < n; ++i)
+    if (!isFixed(i)) {
+      index_map[i] = index;
+      index++;
+    }
+  size_t nvars = index;
+
+  size_t n_smoothing = n - 1;
+  Eigen::MatrixXd A(samples.size() + n_smoothing, nvars);
+  Eigen::MatrixXd b(samples.size() + n_smoothing, 3);
+
+  A.setZero(); b.setZero();
+
+  auto addValue = [&](size_t row, size_t i, double x) {
+    if (isFixed(i))
+      b.row(row) -= VecMap(cpts[i].data()) * x;
+    else
+      A(row, index_map.at(i)) = x;
+  };
+
+  index = 0;
+  for (size_t i = 0; i <= resolution; ++i) {
+    double u = (double)i / resolution;
+    size_t span = basis.findSpan(u);
+    DoubleVector coeff;
+    basis.basisFunctions(span, u, coeff);
+    b.row(index) = VecMap(samples[index].data());
+    for (size_t k = 0; k <= p; ++k) {
+      size_t i1 = span - p + k;
+      addValue(index, i1, coeff[k]);
+    }
+    index++;
+  }
+
+  // Smoothness terms
+  for (size_t i = 1; i < n - 1; ++i) {
+    addValue(index, i, smoothness);
+    addValue(index, i - 1, -0.5 * smoothness);
+    addValue(index, i + 1, -0.5 * smoothness);
+    index++;
+  }
+
+  Eigen::MatrixXd x = A.colPivHouseholderQr().solve(b);
+
+  for (size_t i = 0; i < n; ++i)
+    if (!isFixed(i)) {
+      size_t k = index_map.at(i);
+      cpts[i] = Point3D(x(k, 0), x(k, 1), x(k, 2));
+    }
+}
+
 void bsplineFit(BSSurface &surface, size_t resolution, const PointVector &samples,
                 const std::function<bool(size_t,size_t)> &isFixed, double smoothness) {
   size_t pu = surface.basisU().degree(), pv = surface.basisV().degree();
--- bspline-fit.hh
+++ bspline-fit.hh
@@ -4,6 +4,10 @@
 
 #include <geometry.hh>
 
+void bsplineFit(Geometry::BSCurve &curve, size_t resolution,
+                const Geometry::PointVector &samples,
+                const std::function<bool(size_t)> &isFixed, double smoothness);
+
 void bsplineFit(Geometry::BSSurface &surface, size_t resolution,
                 const Geometry::PointVector &samples,
                 const std::function<bool(size_t,size_t)> &isFixed, double smoothness);
